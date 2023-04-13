c08b230ec25cd2462e84006d376d5d6cda4fb20c
aisancesg <129902469+aisancesg@users.noreply.github.com>
2023-04-13 21:13:23 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,16 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1407630156 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    from scipy.stats import permutation_test
+    alpha = 0.04
+    res = permutation_test((x, y), lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis), 
+                 vectorized=True, 
+                 n_resamples=5000,
+                 alternative='less').pvalue
+    return res < alpha # Ваш ответ, True или False
